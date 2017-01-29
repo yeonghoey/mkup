4ac3ba564e0cce66e6f452113f86b9a224623521
yeonghoey <yeonghoey@gmail.com>
2017-01-29 13:26:37 +0900

    Refactor test_links

--- tests/test_links.py
+++ tests/test_links.py
@@ -4,11 +4,30 @@ from __future__ import absolute_import, unicode_literals
 from orgtools.imgrefs import extract
 
 
+SAMPLES = [
+    '',
+    '[[file.txt]]',
+    '[[image.png][description]]',
+    '[[http://example.com][example]] [[image.png]]',
+    '''* Test
+    - [[path/to/image.jpg]]
+    - [[file:path/to/image.jpeg]]
+    - [[http://example.com][Example]] Site.
+    ** Nested
+    - [[/more/image.gif][gif?]]
+    - [[file:/more/image.gif][gif?]]
+    ''',
+]
+
+
 def test_extract():
-    assert extract('') == []
-    assert extract('[[file.txt]]') == ['file.txt']
-    assert extract('[[image.png][description]]') == ['image.png']
-    assert extract('''
-        [[http://example.org][example]]
-        [[image.png]]
-        ''') == ['http://example.org', 'image.png']
+    expectations = [
+        [],
+        ['file.txt'],
+        ['image.png'],
+        ['http://example.com', 'image.png'],
+        ['path/to/image.jpg', 'file:path/to/image.jpeg', 'http://example.com',
+         '/more/image.gif', 'file:/more/image.gif'],
+    ]
+    for s, e in zip(SAMPLES, expectations):
+        assert e == extract(s)
