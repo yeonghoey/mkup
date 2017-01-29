126e33d65324f2c8a1312638dbeadf798fc030f1
yeonghoey <yeonghoey@gmail.com>
2017-01-29 09:48:18 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""
+orgtools streamlines managing org documents
+"""
+from setuptools import setup, find_packages
+
+
+setup(
+    name='orgtools',
+    version='0.0.1',
+    description=__doc__,
+    author='Yeongho Kim',
+    author_email='yeonghoey@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    keywords='productivity',
+    packages=find_packages(exclude=['tests']),
+    )
